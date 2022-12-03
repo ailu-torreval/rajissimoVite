a125daa353a5128dde82240be5184452bbdc6819
ailu-torreval <ailu.torreval@gmail.com>
2022-12-03 12:42:35 +0100

    migrating site - tailwind connected now

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ["Poppins", "sans-serif"],
+        header: ["Titan One", "cursive"],
+      },
+      colors: {
+        cyan: "#08B7B3",
+        blue: "#104057",
+        yellow: "#FCAF3E",
+        darkyellow: "#A6680A",
+      },
+      animation: {
+        "spin-slow": "spin 10s linear infinite",
+      },
+    },
   },
   plugins: [],
-}
+};
