82f19fdcc465a636e26c6bffcdb1a7d42a217e2d
ailu-torreval <ailu.torreval@gmail.com>
2022-12-03 13:37:14 +0100

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}", 
+    "./index.html",
     "./src/components/**/*.{js,jsx,ts,tsx}",
     "./src/pages/**/*.{js,jsx,ts,tsx}", 
     "./src/sections/**/*.{js,jsx,ts,tsx}", 
