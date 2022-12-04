37718090cbeaa95211ff35af0c021e41eb5ed613
ailu-torreval <ailu.torreval@gmail.com>
2022-12-04 22:30:18 +0100

    ailin - working on product page

--- src/sections/ProductGrid.jsx
+++ src/sections/ProductGrid.jsx
@@ -20,9 +20,9 @@ function ProductGrid(props) {
         {props.cat.map((cat, index) => 
         <div key={index} id={generateId(index)}>
             <h2>{cat} </h2>
-            <div className="flex flex-col gap-2" >{productsData.map((prod) => {
+            <div className="flex flex-col gap-2" >{productsData.map((prod, i) => {
                 if(prod.productType === index + 1 ) {
-                    return <ProductCard key={prod.id} prod={prod} prodId={prodId} setProdId={setProdId} setShowProd={setShowProd} />
+                    return <ProductCard key={i} prod={prod} prodId={prodId} setProdId={setProdId} setShowProd={setShowProd} />
                 }
             })}</div>
         </div>
