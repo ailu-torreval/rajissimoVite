6d7f37010d6f157bacc2d2bae7b0dd609517f888
ailu-torreval <ailu.torreval@gmail.com>
2022-12-08 21:58:50 +0100

    tests finished- url images error found

--- src/components/GameBanner.jsx
+++ src/components/GameBanner.jsx
@@ -3,8 +3,6 @@ import React from 'react'
 function GameBanner() {
   return (
     <div>GameBanner
-
-      <img src="https://ailu-torreval.github.io/rajissimo-assets/hero.png" alt="" />
     </div>
   )
 }
