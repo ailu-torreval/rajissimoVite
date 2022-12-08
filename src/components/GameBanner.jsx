79ccf0a087476f9935c16e55fbeec01112009408
ailu-torreval <ailu.torreval@gmail.com>
2022-12-08 21:41:03 +0100

    test1

--- src/components/GameBanner.jsx
+++ src/components/GameBanner.jsx
@@ -4,7 +4,7 @@ function GameBanner() {
   return (
     <div>GameBanner
 
-      <img src="http://mitproject.dk/pictures/prod-1.jpeg" alt="" />
+      <img src="https://upload.wikimedia.org/wikipedia/commons/2/22/Alcatraz_recreation_yard%2C_NW_view.jpg" alt="" />
     </div>
   )
 }
