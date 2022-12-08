b21498f5595f866011338df1526f9760e73da4b6
ailu-torreval <ailu.torreval@gmail.com>
2022-12-08 21:51:04 +0100

    test2

--- src/components/GameBanner.jsx
+++ src/components/GameBanner.jsx
@@ -4,7 +4,7 @@ function GameBanner() {
   return (
     <div>GameBanner
 
-      <img src="https://upload.wikimedia.org/wikipedia/commons/2/22/Alcatraz_recreation_yard%2C_NW_view.jpg" alt="" />
+      <img src="https://ailu-torreval.github.io/rajissimo-assets/hero.png" alt="" />
     </div>
   )
 }
