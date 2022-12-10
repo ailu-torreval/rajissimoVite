5c36551cd06b675e1c10cacd5bd96b1911ed5406
Marina Tancau <iuli0572@stud.kea.dk>
2022-12-10 13:31:56 +0100

    Marina: rounded borders added

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -17,12 +17,13 @@ function MenuCard(props) {
         {props.prod.title}
       </h3>
       <div className="flex flex-col md:flex md:flex-row md:gap-4 lg:gap-12 lg:px-12">
-        <div className="md:basis-1/3 relative corner-yellow">
+        <div className="md:basis-1/3 relative md:border md:border-2 md:border-[#fee3bb] md:pt-[10px] md:pl-[10px] md:rounded-[24px] corner-yellow max-w-[350px] m-auto md:m-0">
           <img
             src={props.foto}
             alt=""
-            className="hidden md:block md:rounded-[20px] md:h-full md:w-auto md:object-cover absolute corner-blue"
+            className=" rounded-[20px] md:px-0 mb-4 md:m-0 md:block md:h-full md:w-auto md:object-cover md:absolute"
           ></img>
+          <div className="md:border md:boder-2 md:border-[#08b7b3] md:rounded-[24px] md:h-full md:w-full md:absolute md:top-[20px] md:-right-[20px] md:-z-10"></div>
         </div>
         <div className="flex flex-col justify-center items-center gap-4 md:gap-10">
           <div className="grid gap-4 sm:grid-cols-2 justify-center md:basis-2/3 md:align-start md:items-start md:text-center md:gap-8">
